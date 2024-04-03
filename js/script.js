1f158a736b9b5ea1a7161744660339597733fe1e
AngeloC89 <Angelociulla89@gmail.com>
2024-04-03 11:16:56 +0200

    created macro structure

---
+++ js/script.js
@@ -0,0 +1,26 @@
+import { contacts } from './data.js';
+
+const { createApp } = Vue;
+
+
+
+//option object.
+createApp({
+  data() { //metodo data()
+    return {
+     
+    }
+  },
+  methods: {
+  
+
+  },
+  computed: {
+  
+  },
+  mounted() {
+   
+
+  },
+
+}).mount('#app')
