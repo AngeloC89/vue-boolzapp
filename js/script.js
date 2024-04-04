c70c19d5bf194ffa72e7c75296eed0583e81bbd0
AngeloC89 <Angelociulla89@gmail.com>
2024-04-04 14:12:25 +0200

    I added on computed serachList for search the contact on my list and when i write a message, return reply ok.

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ createApp({
       contacts,
       activeId: 1,
       chatIn: '',
+      searchItem: '',
 
     }
   },
@@ -20,28 +21,38 @@ createApp({
     clickActiveItem(id) {
       return this.activeId = id
     },
-    createMessag(msg, status){
+    createMessag(msg, status) {
       const newObj = {
         date: dt.now().setLocale('it').toFormat('dd/MM/yyyy HH:mm:ss'),
-         message: msg,
-         status: status,
-       } 
-       return newObj
+        message: msg,
+        status: status,
+      }
+      return newObj
     },
     newMessag() {
+      if (this.chatIn.trim() === '') {
+        return;
+      }
       const msg = this.createMessag(this.chatIn, 'sent');
       this.activeItem.messages.push(msg);
       this.chatIn = ''
-     
-     
+      setTimeout(() => {
+        this.activeItem.messages.push(this.createMessag('ok', 'received'));
 
-      
+      }, 1000);
+
     },
 
   },
   computed: {
     activeItem() {
       return this.contacts.find((el) => el.id === this.activeId)
+    },
+    searchList() {
+      return this.contacts.filter((el) => el.name.toLowerCase().includes(this.searchItem.toLowerCase()));
+      
+      
+
     },
 
 
