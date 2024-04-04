38405f9264a3eb26e0c22356a9ded1f49800cfbf
AngeloC89 <Angelociulla89@gmail.com>
2024-04-04 13:16:41 +0200

    I can send a message with sent class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import { contacts } from './data.js';
 
+const dt = luxon.DateTime;
+
 const { createApp } = Vue;
 
 
@@ -10,25 +12,43 @@ createApp({
     return {
       contacts,
       activeId: 1,
-     
+      chatIn: '',
+
     }
   },
   methods: {
-    clickActiveItem(id){
+    clickActiveItem(id) {
       return this.activeId = id
-      
-  },
-},
-  computed: {
-    activeItem(){
-      return this.contacts.find((el)=> el.id === this.activeId)
     },
-   
-  
+    createMessag(msg, status){
+      const newObj = {
+        date: dt.now().setLocale('it').toFormat('dd/MM/yyyy HH:mm:ss'),
+         message: msg,
+         status: status,
+       } 
+       return newObj
+    },
+    newMessag() {
+      const msg = this.createMessag(this.chatIn, 'sent');
+      this.activeItem.messages.push(msg);
+      this.chatIn = ''
+     
+     
+
+      
+    },
+
+  },
+  computed: {
+    activeItem() {
+      return this.contacts.find((el) => el.id === this.activeId)
+    },
+
+
   },
   mounted() {
     console.log(contacts)
-   
+
 
   },
 
